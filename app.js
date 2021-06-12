5d75e08882fbe892f1aaf3fa5e2dba836cc1d635
andi-s0106 <andress.933110@gmail.com>
2021-06-12 21:33:43 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const getEmail = require('./getEmail.js');
+const member = require('./member.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const SmeeClient = require('smee-client')
+
+const smeeStar = ()=> {
+
+    const smee = new SmeeClient({
+        source: 'https://smee.io/TDckB0c9vfal9Etm',
+        target: 'http://localhost:3000/events',
+        logger: console
+    })
+
+    const events = smee.start()
+
+}
+
+smeeStar();
 
 const app = express();
 
@@ -12,13 +27,19 @@ app.use(bodyParser.json());
 app.post('/events', function (req, res) {
   const temp = req.body.action.display.translationKey;
   const  id = req.body.action.idMemberCreator
-  console.log(id);
-  if (temp == 'action_move_card_from_list_to_list')
+  const listBefore = req.body.action.data.listBefore;
+  const listAfter = req.body.action.data.listAfter;
+  const idcard = req.body.action.data.card.id;
+  if (temp == 'action_move_card_from_list_to_list' && listBefore.name == 'Lista de tareas' && listAfter.name == 'En proceso')
   {
-    getEmail(id);
+    console.log('Request received');
+    member(id);
   };
 });
 
 app.listen(port, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
+
+
+//module.exports = { id }
