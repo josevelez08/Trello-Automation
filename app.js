9cced9b9519e302f4c9b5f06ccfbead80d6ec20e
andi-s0106 <andress.933110@gmail.com>
2021-06-12 21:36:52 +0000

    updated app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.post('/events', function (req, res) {
   const  id = req.body.action.idMemberCreator
   const listBefore = req.body.action.data.listBefore;
   const listAfter = req.body.action.data.listAfter;
-  if (temp == 'action_move_card_from_list_to_list' && listBefore.name == 'En proceso' && listAfter.name == 'Lista de tareas')
+  if (temp == 'action_move_card_from_list_to_list' && listBefore.name == 'Lista de tareas' && listAfter.name == 'En proceso')
   {
     console.log('We got');
-    // getEmail(id);
+    member(id);
   };
 });
 
