c130b1b0934aac5399cc865d948e93d379c313aa
josevelez08 <jose-0919@hotmail.com>
2021-06-12 19:21:56 -0500

    add url again

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@
 // https://www.npmjs.com/package/node-fetch
 const fetch = require('node-fetch');
 
-fetch('https://api.trello.com/1/webhooks/?key=09b33908b544abe20cb679d61f9c6026&token=ddb87243d73f97e299323415fc5068b122803e3c8dcda3a19084f954dab40c6b&callbackURL=https://git.heroku.com/trello-pandadocs-integration.git&idModel=60a9728ddc9be2860f10b48b', {
+fetch('https://api.trello.com/1/webhooks/?key=09b33908b544abe20cb679d61f9c6026&token=ddb87243d73f97e299323415fc5068b122803e3c8dcda3a19084f954dab40c6b&callbackURL=https://trello-pandadocs-integration.herokuapp.com/&idModel=60a9728ddc9be2860f10b48b', {
   method: 'POST',
   headers: {
     'Accept': 'application/json'
