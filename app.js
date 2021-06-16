313d8d8c3d5baa81f1a917037bb8ef63a40d2552
josevelez08 <jose-0919@hotmail.com>
2021-06-16 10:22:58 -0500

    google html verification

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.post('/events', function (req, res) {
   // {
   // };
 });
-
+app.get('/googlea08991f6f71af58f',function(req,res) {
+  res.sendFile('google.html');
+});
 app.head('/events', function (req, res) {
   console.log('Request head', req.body);
   res.status(200)
