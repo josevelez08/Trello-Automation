4b38c54be43f1088ea7b05906a56256c20e9ddce
josevelez08 <jose-0919@hotmail.com>
2021-06-17 11:06:56 -0500

    add /landing-page html

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.head('/events', function (req, res) {
   res.status(200)
   res.send({});
 });
-
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 app.get('/events', function (req, res) {
   res.status(200)
   res.send({});
